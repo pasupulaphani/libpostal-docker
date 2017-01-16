2675e89155baee6aa70db4947f1bdd306c4ddbec
Phaninder Pasupula <pasupulaphani@gmail.com>
2017-01-16 16:00:52 +0000

    added server

--- zeromq/server.js
+++ zeromq/server.js
@@ -1,7 +1,24 @@
 import zmq from "zmq";
+import libPostalParser from "../libPostalParser"
+import logToConsole from "../libPostalParser/util/log_to_console"
 
-const sock = zmq.socket("pub");
+
+const socket = zmq.socket("pull");
 const port = process.ENV.PORT || "4242";
 
-sock.bindSync(`tcp://127.0.0.1:${port}`);
-console.log(`Publisher bound to port ${port}`);
+
+// Add a callback for the event that is invoked when we receive a message.
+socket.on("message", function (method, text) {
+  logToConsole("Received message: " + message.toString("utf8"));
+
+  return libPostalParser[method](text)
+});
+
+socket.bindSync(`tcp://127.0.0.1:${port}`, function (error) {
+  if (error) {
+    logToConsole("Failed to bind socket: " + error.message);
+    process.exit(0);
+  } else {
+    logToConsole(`Responder bound to port ${port}`);
+  }
+});
